d85a043f677996f09323fe05daf058519fbc4603
milenards <94084122+milenards@users.noreply.github.com>
2022-08-24 19:24:50 -0300

    salvando dados

--- JAVASCRIPT/calculadora.js
+++ JAVASCRIPT/calculadora.js
@@ -1,16 +1,41 @@
-function calcular(oper, valor1, valor2) {
-    if (oper === "somar") {
-       var valor = parseFloat(valor1) + parseFloat(valor2);
+function calcular() {
+
+   let valor1 = parseFloat(document.querySelector("#num1").value);
+   let valor2 = parseFloat(document.querySelector("#num2").value); 
+   let oper  = document.querySelector("#selecao").value;
+
+   console.log(valor1);
+   console.log(valor2);
+   console.log(oper);
+
+}
+
+
+   /* if (oper === "soma") {
+        resultado = parseFloat(valor1) + parseFloat(valor2);
     } else {
        if (oper === "subtrair") {
-          var valor = valor1 - valor2;
+           resultado = valor1 - valor2;
        } else {
           if (oper === "multiplicar") {
-             var valor = valor1 * valor2;
+              resultado = valor1 * valor2;
           } else {
-             var valor = valor1 / valor2;
+              resultado = valor1 / valor2;
           }
        }
+       */
+    
+    document.querySelector("#resultado").innerHTML = resultado
+    //console.log(resultado)
+   
+   /* const operations = {
+      soma: (a,b) => a+b,
+      sub: (a,b) => a-b,
+      mult: (a,b) => a*b,
+      divi: (a,b) => a/b,
     }
-    console.log(valor)
- }
+    const resultado = operations[operations](a, b)*/
+
+   
+    
+ 
---
+++ JAVASCRIPT/soma.js
@@ -0,0 +1,6 @@
+let objeto = {
+    nome: function soma (a,b) { a + b},
+    idade: 35
+}
+
+objeto[nome](a,b)
